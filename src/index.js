b8483f77fc673da055f0b3db8c5930e10a22ddb9
Andrei Soloviov <solovyev833@gmail.com>
2021-03-10 21:25:30 +0300

    Step 1 complete

--- src/index.js
+++ src/index.js
@@ -4,31 +4,14 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import '../assets/application.scss';
-
+// @ts-ignore
+// eslint-disable-next-line import/no-unresolved
 import gon from 'gon';
+import app from './App.jsx';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-const p = document.createElement('p');
-p.classList.add('card-text');
-p.textContent = 'It works!';
-
-const h5 = document.createElement('h5');
-h5.classList.add('card-title');
-h5.textContent = 'Project frontend l4 boilerplate';
-
-const cardBody = document.createElement('div');
-cardBody.classList.add('card-body');
-cardBody.append(h5, p);
-
-const card = document.createElement('div');
-card.classList.add('card', 'text-center');
-card.append(cardBody);
-
 const container = document.querySelector('#chat');
-container.append(card);
-
-console.log('it works!');
-console.log('gon', gon);
+app(container, gon);
