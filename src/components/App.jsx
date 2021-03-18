265c93cc9a1fb11e5138b8658cc2476e68c5e688
Andrei Soloviov <solovyev833@gmail.com>
2021-03-18 09:41:01 +0300

    Put all init logic in one component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,19 @@ import 'regenerator-runtime/runtime';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-
+import { io } from 'socket.io-client';
 import faker from 'faker';
 import Cookies from 'js-cookie';
-import Chat from './Chat.jsx';
+import {
+  Row, Col,
+} from 'react-bootstrap';
+import ChannelNav from './ChannelNav.jsx';
+import Messages from './Messages.jsx';
+import MessageForm from './MessageForm.jsx';
+import messagesReducer, { addMessage } from '../slices/messages.js';
+import channelsReducer, { addChannel, renameChannel, removeChannel } from '../slices/channels.js';
 import AppContext from '../app-context.js';
-import channelsReducer from '../slices/channels.js';
-import messagesReducer from '../slices/messages.js';
+
 import currentChannelIdReducer from '../slices/currentChannelId.js';
 
 const setUserNameIfEmpty = () => {
@@ -31,11 +37,26 @@ const App = ({ gon }) => {
     },
     preloadedState: gon,
   });
+  const { dispatch } = store;
+
+  const socket = io();
+  socket.on('newMessage', ({ data }) => dispatch(addMessage(data)));
+  socket.on('newChannel', ({ data }) => dispatch(addChannel(data)));
+  socket.on('renameChannel', ({ data }) => dispatch(renameChannel(data)));
+  socket.on('removeChannel', ({ data }) => dispatch(removeChannel(data)));
 
   return (
     <Provider store={store}>
       <AppContext.Provider value={setUserNameIfEmpty()}>
-        <Chat />
+        <Row className="h-100 pb-3">
+          <Col className="border-right" xs={3}>
+            <ChannelNav />
+          </Col>
+          <Col className="d-flex flex-column justify-content-end h-100">
+            <Messages />
+            <MessageForm />
+          </Col>
+        </Row>
       </AppContext.Provider>
     </Provider>
   );
