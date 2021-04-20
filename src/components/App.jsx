a8e46c73a5cf56fed8f0c1c79100b1d86cd23cf2
Andrei Soloviov <solovyev833@gmail.com>
2021-04-20 08:50:01 +0300

    State now loads correctly after the first login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,5 @@
 import axios from 'axios';
-import _ from 'lodash';
 import React, { useState, useEffect } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import {
@@ -21,7 +19,6 @@ import LogInForm from './LogInForm.jsx';
 import SignUpForm from './SignUpForm.jsx';
 import Chat from './Chat.jsx';
 import NoMatch from './NoMatch.jsx';
-import { authContext } from '../contexts/index.jsx';
 import { useAuth, useSocket } from '../hooks/index.jsx';
 import messagesReducer, { addMessage } from '../slices/messages.js';
 import channelsReducer, { addChannel, renameChannel, removeChannel } from '../slices/channels.js';
@@ -38,38 +35,6 @@ i18n
     lng: 'ru',
     fallbackLng: 'ru',
   });
-
-const ErrorFallback = ({ error, resetErrorBoundary }) => (
-  <div role="alert">
-    <p>Something went wrong:</p>
-    <pre>{error.message}</pre>
-    <button type="button" onClick={resetErrorBoundary}>Try again</button>
-  </div>
-);
-
-const AuthProvider = ({ children }) => {
-  const isLoggedIn = _.has(localStorage, 'hexletChatUserId');
-  const [loggedIn, setLoggedIn] = useState(isLoggedIn);
-  const [username, setUsername] = useState(null);
-
-  const logIn = (name) => {
-    setUsername(name);
-    setLoggedIn(true);
-  };
-  const logOut = () => {
-    localStorage.removeItem('hexletChatUserId');
-    setLoggedIn(false);
-  };
-
-  return (
-    <authContext.Provider value={{
-      username, loggedIn, logIn, logOut,
-    }}
-    >
-      {children}
-    </authContext.Provider>
-  );
-};
 
 const PrivateRoute = ({ children, path }) => {
   const auth = useAuth();
@@ -113,7 +78,7 @@ const getAuthHeader = () => {
   return {};
 };
 
-export default ({ rollbar }) => {
+export default () => {
   const [data, setData] = useState({
     channels: [],
     messages: [],
@@ -124,7 +89,7 @@ export default ({ rollbar }) => {
       channelName: null,
     },
   });
-
+  const auth = useAuth();
   useEffect(() => {
     const authHeader = getAuthHeader();
     async function getData() {
@@ -136,7 +101,7 @@ export default ({ rollbar }) => {
       setData((prevState) => ({ ...prevState, ...res.data }));
     }
     getData();
-  }, []);
+  }, [auth.loggedIn]);
 
   const store = configureStore({
     reducer: {
@@ -157,43 +122,35 @@ export default ({ rollbar }) => {
 
   const handleSelect = (eventKey) => i18n.changeLanguage(eventKey);
 
-  const logError = (error) => {
-    console.log(error);
-    rollbar.error(error);
-  };
-
   return (
     <div className="d-flex flex-column h-100">
-      <ErrorBoundary FallbackComponent={ErrorFallback} onError={logError}>
-        <AuthProvider>
-          <Router>
-            <Navbar className="mb-3" expand="lg">
-              <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
-              <NavDropdown className="ml-auto" id="nav-dropdown" title="&#127758;" onSelect={handleSelect}>
-                <NavDropdown.Item eventKey="ru">RU</NavDropdown.Item>
-                <NavDropdown.Item eventKey="en">EN</NavDropdown.Item>
-              </NavDropdown>
-              <LogOutButton />
-            </Navbar>
-            <Switch>
-              <PrivateRoute exact path="/">
-                <Provider store={store}>
-                  <Chat />
-                </Provider>
-              </PrivateRoute>
-              <AuthRoute exact path="/login">
-                <LogInForm />
-              </AuthRoute>
-              <AuthRoute exact path="/signup">
-                <SignUpForm />
-              </AuthRoute>
-              <Route path="*">
-                <NoMatch />
-              </Route>
-            </Switch>
-          </Router>
-        </AuthProvider>
-      </ErrorBoundary>
+      <Router>
+        <Navbar className="mb-3" expand="lg">
+          <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
+          <NavDropdown className="ml-auto" id="nav-dropdown" title="&#127758;" onSelect={handleSelect}>
+            <NavDropdown.Item eventKey="ru">RU</NavDropdown.Item>
+            <NavDropdown.Item eventKey="en">EN</NavDropdown.Item>
+          </NavDropdown>
+          <LogOutButton />
+        </Navbar>
+        <Switch>
+          <PrivateRoute exact path="/">
+            <Provider store={store}>
+              <Chat />
+            </Provider>
+          </PrivateRoute>
+          <AuthRoute exact path="/login">
+            <LogInForm />
+          </AuthRoute>
+          <AuthRoute exact path="/signup">
+            <SignUpForm />
+          </AuthRoute>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -4,15 +4,41 @@ import 'regenerator-runtime/runtime.js';
 import 'core-js/stable/index.js';
 import '../assets/application.scss';
 import { io } from 'socket.io-client';
-import React from 'react';
+import _ from 'lodash';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { ErrorBoundary } from 'react-error-boundary';
 import Rollbar from 'rollbar';
 import App from './components/App.jsx';
-import { socketContext } from './contexts/index.jsx';
+import { socketContext, authContext } from './contexts/index.jsx';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
+
+const AuthProvider = ({ children }) => {
+  const isLoggedIn = _.has(localStorage, 'hexletChatUserId');
+  const [loggedIn, setLoggedIn] = useState(isLoggedIn);
+  const [username, setUsername] = useState(null);
+
+  const logIn = (name) => {
+    setUsername(name);
+    setLoggedIn(true);
+  };
+  const logOut = () => {
+    localStorage.removeItem('hexletChatUserId');
+    setLoggedIn(false);
+  };
+
+  return (
+    <authContext.Provider value={{
+      username, loggedIn, logIn, logOut,
+    }}
+    >
+      {children}
+    </authContext.Provider>
+  );
+};
 
 const rollbar = new Rollbar(
   {
@@ -25,12 +51,29 @@ const rollbar = new Rollbar(
   },
 );
 
+const logError = (error) => {
+  console.log(error);
+  rollbar.error(error);
+};
+
+const ErrorFallback = ({ error, resetErrorBoundary }) => (
+  <div role="alert">
+    <p>Something went wrong:</p>
+    <pre>{error.message}</pre>
+    <button type="button" onClick={resetErrorBoundary}>Try again</button>
+  </div>
+);
+
 const socket = io();
 
 const container = document.querySelector('#chat');
 ReactDOM.render(
-  <socketContext.Provider value={socket}>
-    <App rollbar={rollbar} />
-  </socketContext.Provider>,
+  <ErrorBoundary FallbackComponent={ErrorFallback} onError={logError}>
+    <AuthProvider>
+      <socketContext.Provider value={socket}>
+        <App />
+      </socketContext.Provider>
+    </AuthProvider>
+  </ErrorBoundary>,
   container,
 );
