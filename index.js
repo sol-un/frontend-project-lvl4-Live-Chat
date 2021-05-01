35cc249b365459f86a68dd702acd2d89cc44bac1
Andrei Soloviov <solovyev833@gmail.com>
2021-05-01 13:08:33 +0300

    Add i18n instance

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import i18n from 'i18next';
+import i18next from 'i18next';
 import { initReactI18next, I18nextProvider } from 'react-i18next';
 import ru from './src/locales/ru.js';
 import en from './src/locales/en.js';
@@ -14,29 +14,30 @@ import currentChannelIdReducer from './src/slices/currentChannelId.js';
 import uiStateReducer from './src/slices/uiState.js';
 import { socketContext } from './src/contexts/index.jsx';
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-      ru,
-      en,
+const init = (socket) => {
+  const i18nextInstance = i18next.createInstance();
+  i18nextInstance
+    .use(initReactI18next)
+    .init({
+      resources: {
+        ru,
+        en,
+      },
+      lng: 'ru',
+      fallbackLng: 'ru',
+    });
+
+  const store = configureStore({
+    reducer: {
+      channels: channelsReducer,
+      messages: messagesReducer,
+      currentChannelId: currentChannelIdReducer,
+      uiState: uiStateReducer,
     },
-    lng: 'ru',
-    fallbackLng: 'ru',
   });
 
-const store = configureStore({
-  reducer: {
-    channels: channelsReducer,
-    messages: messagesReducer,
-    currentChannelId: currentChannelIdReducer,
-    uiState: uiStateReducer,
-  },
-});
+  const { dispatch } = store;
 
-const { dispatch } = store;
-
-const init = (socket) => {
   socket.on('newMessage', (response) => dispatch(addMessage(response)));
   socket.on('newChannel', (response) => dispatch(addChannel(response)));
   socket.on('renameChannel', (response) => dispatch(renameChannel(response)));
@@ -45,7 +46,7 @@ const init = (socket) => {
   return (
     <Provider store={store}>
       <socketContext.Provider value={socket}>
-        <I18nextProvider i18n={i18n}>
+        <I18nextProvider i18n={i18nextInstance}>
           <App />
         </I18nextProvider>
       </socketContext.Provider>
